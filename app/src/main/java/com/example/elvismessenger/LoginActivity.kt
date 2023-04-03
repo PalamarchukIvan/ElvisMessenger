b001520b258dec9339f3b74defe1c32837712a7d
Palamarchuk Ivan <vanavana978@gmail.com>
2023-04-03 20:22:32 +0300

    Added logging + registration BETA

---
+++ app/src/main/java/com/example/elvismessenger/LoginActivity.kt
@@ -0,0 +1,27 @@
+package com.example.elvismessenger
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.elvismessenger.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.loginButtonLogin.setOnClickListener {
+            val login = binding.loginPlainTextLogin.text.toString()
+            val password = binding.passwordPlainTextLogin.text.toString()
+            //Тут будет переход к тебе
+        }
+
+        binding.noAccauntTextLogin.setOnClickListener {
+            val i = Intent(this, RegistrationActivity::class.java)
+            startActivity(i)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/elvismessenger/RegistrationActivity.kt
@@ -0,0 +1,28 @@
+package com.example.elvismessenger
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.elvismessenger.databinding.ActivityRegistrationBinding
+
+class RegistrationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityRegistrationBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+        binding.regestrateButtonRegestration.setOnClickListener {
+            val password1 = binding.password1PlainTextRegistration.text.toString()
+            val password2 = binding.password2PlainTextRegistration.text.toString()
+            if(password1 != password2) {
+                Toast.makeText(this, "Passwords must be the same", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            val userName = binding.userNamePlainTextRegistration.text.toString()
+            val login = binding.loginPlainTextRegistration.text.toString()
+            Toast.makeText(this, "GOOD: $userName   $login   $password1", Toast.LENGTH_SHORT).show()
+            //Переход к тебе
+        }
+    }
+}
