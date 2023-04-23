d325e1b2156f88a2e6c56cfc3934600977f33c3c
Palamarchuk Ivan <vanavana978@gmail.com>
2023-04-23 18:42:50 +0300

    Added settings via Preferences and added color main theme

--- app/src/main/java/com/example/elvismessenger/fragments/ChatListFragment.kt
+++ app/src/main/java/com/example/elvismessenger/fragments/ChatListFragment.kt
@@ -1,9 +1,12 @@
 package com.example.elvismessenger.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
+import androidx.preference.PreferenceManager
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -36,6 +39,42 @@ class ChatListFragment : Fragment(R.layout.fragment_chat_list) {
             name.putString("name", it.name)
             Navigation.findNavController(view).navigate(R.id.action_chatListFragment_to_chatLogFragment, name)
         }
+
+        loadSettingsData()
+    }
+
+    private fun loadSettingsData() {
+        val sharedPreference = PreferenceManager.getDefaultSharedPreferences(requireContext())
+
+        val name = sharedPreference.getString("name", "")
+        Log.d("settings test", "1")
+        val about = sharedPreference.getString("about", "")
+        Log.d("settings test", "2")
+
+        val email = sharedPreference.getString("email", "")
+        Log.d("settings test", "3")
+        val password = sharedPreference.getString("password", "")
+        Log.d("settings test", "4")
+        val ifCanSpy = sharedPreference.getBoolean("spying", false);
+        Log.d("settings test", "5")
+
+        val language = sharedPreference.getString("language", "english")
+        Log.d("settings test", "6")
+        val volume = sharedPreference.getInt("volume", 40)
+        Log.d("settings test", "7")
+        val textSize = sharedPreference.getString("text_size", "")
+        Log.d("settings test", "8")
+        val ifDarkTheme = sharedPreference.getBoolean("theme", false)
+        Log.d("settings test", "9")
+
+        Toast.makeText(context, "name: $name", Toast.LENGTH_SHORT).show()
+        Toast.makeText(context, "about: $about", Toast.LENGTH_SHORT).show()
+        Toast.makeText(context, "email: $email", Toast.LENGTH_SHORT).show()
+        Toast.makeText(context, "password: $password", Toast.LENGTH_SHORT).show()
+        Toast.makeText(context, "language: $language", Toast.LENGTH_SHORT).show()
+        Toast.makeText(context, "volume: $volume", Toast.LENGTH_SHORT).show()
+        Toast.makeText(context, "text size: $textSize", Toast.LENGTH_SHORT).show()
+        Toast.makeText(context, "theme is dak?: $ifDarkTheme", Toast.LENGTH_SHORT).show()
     }
 
     object FakeChat {
--- app/src/main/java/com/example/elvismessenger/fragments/SettingsFragment.kt
+++ app/src/main/java/com/example/elvismessenger/fragments/SettingsFragment.kt
@@ -1,6 +1,15 @@
 package com.example.elvismessenger.fragments
 
-import androidx.fragment.app.Fragment
+import android.os.Bundle
+import android.view.Menu
+import androidx.preference.PreferenceCategory
+import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.PreferenceManager
 import com.example.elvismessenger.R
 
-class SettingsFragment : Fragment(R.layout.fragment_settings)
+class SettingsFragment : PreferenceFragmentCompat() {
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.root_preferences, rootKey)
+    }
+}
