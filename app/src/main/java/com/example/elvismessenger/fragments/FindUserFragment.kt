b9593bfd8dba486f774e694171d7fe7453af75fd
Palamarchuk Ivan <vanavana978@gmail.com>
2023-04-25 18:48:06 +0300

    Find user was reworked

--- app/src/main/java/com/example/elvismessenger/fragments/FindUserFragment.kt
+++ app/src/main/java/com/example/elvismessenger/fragments/FindUserFragment.kt
@@ -5,6 +5,7 @@ import android.view.Menu
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -20,7 +21,12 @@ class FindUserFragment : Fragment(R.layout.fragment_find_user) {
         val recyclerView: RecyclerView = view.findViewById(R.id.found_user_list_find_user)
 
         // Добавление линии между элементами чата
-        recyclerView.addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.VERTICAL))
+        recyclerView.addItemDecoration(
+            DividerItemDecoration(
+                context,
+                DividerItemDecoration.VERTICAL
+            )
+        )
 
         recyclerView.layoutManager = LinearLayoutManager(context)
 
@@ -29,11 +35,12 @@ class FindUserFragment : Fragment(R.layout.fragment_find_user) {
         recyclerView.adapter = ChatsListAdapter(users)
 
         // Для нахождения юзеров локально
-        val searchButton: Button = view.findViewById(R.id.find_button_find_user)
         val inputText: EditText = view.findViewById(R.id.search_user_edit_text_find_user)
 
-        searchButton.setOnClickListener {
-            recyclerView.adapter = ChatsListAdapter(users.filter {it.name.contains(inputText.text)})
+
+        view.findViewById<EditText>(R.id.search_user_edit_text_find_user).addTextChangedListener {
+            recyclerView.adapter =
+                ChatsListAdapter(users.filter { it.name.contains(inputText.text) })
         }
     }
 }
