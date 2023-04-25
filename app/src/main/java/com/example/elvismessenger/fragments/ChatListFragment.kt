0a5e95ec2beb625b0ac3f56724a9c4a5912afb7a
Palamarchuk Ivan <vanavana978@gmail.com>
2023-04-25 10:33:54 +0300

    commented redundant logic

--- app/src/main/java/com/example/elvismessenger/fragments/ChatListFragment.kt
+++ app/src/main/java/com/example/elvismessenger/fragments/ChatListFragment.kt
@@ -44,37 +44,37 @@ class ChatListFragment : Fragment(R.layout.fragment_chat_list) {
     }
 
     private fun loadSettingsData() {
-        val sharedPreference = PreferenceManager.getDefaultSharedPreferences(requireContext())
-
-        val name = sharedPreference.getString("name", "")
-        Log.d("settings test", "1")
-        val about = sharedPreference.getString("about", "")
-        Log.d("settings test", "2")
-
-        val email = sharedPreference.getString("email", "")
-        Log.d("settings test", "3")
-        val password = sharedPreference.getString("password", "")
-        Log.d("settings test", "4")
-        val ifCanSpy = sharedPreference.getBoolean("spying", false);
-        Log.d("settings test", "5")
-
-        val language = sharedPreference.getString("language", "english")
-        Log.d("settings test", "6")
-        val volume = sharedPreference.getInt("volume", 40)
-        Log.d("settings test", "7")
-        val textSize = sharedPreference.getString("text_size", "")
-        Log.d("settings test", "8")
-        val ifDarkTheme = sharedPreference.getBoolean("theme", false)
-        Log.d("settings test", "9")
-
-        Toast.makeText(context, "name: $name", Toast.LENGTH_SHORT).show()
-        Toast.makeText(context, "about: $about", Toast.LENGTH_SHORT).show()
-        Toast.makeText(context, "email: $email", Toast.LENGTH_SHORT).show()
-        Toast.makeText(context, "password: $password", Toast.LENGTH_SHORT).show()
-        Toast.makeText(context, "language: $language", Toast.LENGTH_SHORT).show()
-        Toast.makeText(context, "volume: $volume", Toast.LENGTH_SHORT).show()
-        Toast.makeText(context, "text size: $textSize", Toast.LENGTH_SHORT).show()
-        Toast.makeText(context, "theme is dak?: $ifDarkTheme", Toast.LENGTH_SHORT).show()
+//        val sharedPreference = PreferenceManager.getDefaultSharedPreferences(requireContext())
+//
+//        val name = sharedPreference.getString("name", "")
+//        Log.d("settings test", "1")
+//        val about = sharedPreference.getString("about", "")
+//        Log.d("settings test", "2")
+//
+//        val email = sharedPreference.getString("email", "")
+//        Log.d("settings test", "3")
+//        val password = sharedPreference.getString("password", "")
+//        Log.d("settings test", "4")
+//        val ifCanSpy = sharedPreference.getBoolean("spying", false);
+//        Log.d("settings test", "5")
+//
+//        val language = sharedPreference.getString("language", "english")
+//        Log.d("settings test", "6")
+//        val volume = sharedPreference.getInt("volume", 40)
+//        Log.d("settings test", "7")
+//        val textSize = sharedPreference.getString("text_size", "")
+//        Log.d("settings test", "8")
+//        val ifDarkTheme = sharedPreference.getBoolean("theme", false)
+//        Log.d("settings test", "9")
+//
+//        Toast.makeText(context, "name: $name", Toast.LENGTH_SHORT).show()
+//        Toast.makeText(context, "about: $about", Toast.LENGTH_SHORT).show()
+//        Toast.makeText(context, "email: $email", Toast.LENGTH_SHORT).show()
+//        Toast.makeText(context, "password: $password", Toast.LENGTH_SHORT).show()
+//        Toast.makeText(context, "language: $language", Toast.LENGTH_SHORT).show()
+//        Toast.makeText(context, "volume: $volume", Toast.LENGTH_SHORT).show()
+//        Toast.makeText(context, "text size: $textSize", Toast.LENGTH_SHORT).show()
+//        Toast.makeText(context, "theme is dak?: $ifDarkTheme", Toast.LENGTH_SHORT).show()
     }
 
     object FakeChat {
