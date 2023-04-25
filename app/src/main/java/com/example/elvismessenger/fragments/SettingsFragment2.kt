435704cf380320596e736b7892aedbb48acff2f3
Palamarchuk Ivan <vanavana978@gmail.com>
2023-04-25 10:33:15 +0300

    Added new settings design (No logic)

--- app/src/main/java/com/example/elvismessenger/fragments/SettingsFragment2.kt
+++ app/src/main/java/com/example/elvismessenger/fragments/SettingsFragment2.kt
@@ -1,13 +1,10 @@
 package com.example.elvismessenger.fragments
 
 import android.os.Bundle
-import android.view.Menu
-import androidx.preference.PreferenceCategory
 import androidx.preference.PreferenceFragmentCompat
-import androidx.preference.PreferenceManager
 import com.example.elvismessenger.R
 
-class SettingsFragment : PreferenceFragmentCompat() {
+class SettingsFragment2 : PreferenceFragmentCompat() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
