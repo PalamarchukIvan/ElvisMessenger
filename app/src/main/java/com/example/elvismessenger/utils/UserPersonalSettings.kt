c5163736abcd49feaf6b306481ba4417e223bbe6
Palamarchuk Ivan <vanavana978@gmail.com>
2023-04-26 12:12:29 +0300

    made primary constructor private

--- app/src/main/java/com/example/elvismessenger/utils/UserPersonalSettings.kt
+++ app/src/main/java/com/example/elvismessenger/utils/UserPersonalSettings.kt
@@ -1,16 +1,12 @@
 package com.example.elvismessenger.utils
 
 
-class UserPersonalSettings {
+class UserPersonalSettings private constructor() {
     var email: String = ""
     var password: String = ""
     var phoneNumber: String = ""
     var ifDarkTheme: Boolean = false
     var textSize: Int = 0
-
-    private constructor() {
-
-    }
 
     companion object {
         private var instance: UserPersonalSettings? = null
